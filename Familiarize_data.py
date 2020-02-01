8ed64a7030dc349a069f1dff92946dfed8aa1f84
Amar Kelu Nair <amarknair7@gmail.com>
2020-01-31 18:06:42 -0800

    Updated model 1

--- Familiarize_data.py
+++ Familiarize_data.py
@@ -8,10 +8,6 @@ dirname = os.path.dirname(__file__)
 
 import pandas as pd
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
 melb_data = pd.read_csv(dirname+'/input/melbourne-housing-snapshot/panda_test.csv')
 print(melb_data.describe())
 print(melb_data.columns)
