f1120aa7edd8572aea5ba300076079d7c41e8c4f
Amar Kelu Nair <amarknair7@gmail.com>
2020-02-02 01:30:41 -0800

    Absolute Mean Error Validation

--- Predict_Hose_Price.py
+++ Predict_Hose_Price.py
@@ -33,3 +33,20 @@ print("Predictions for the following 5 houses")
 print (X.head())
 print("Predictions are ")
 print(melb_model.predict(X.head()))
+
+#In-Sample validation score
+from sklearn.metrics import mean_absolute_error
+
+predicted_home_prices = melb_model.predict(X)
+mean_absolute_error(y, predicted_home_prices)
+
+#Model Validation using absolute mean error
+from sklearn.model_selection import train_test_split
+
+train_X, val_X, train_y, val_y = train_test_split(X,y,test_size=0.30, random_state=0)
+melb_model = DecisionTreeRegressor()
+melb_model.fit(train_X, train_y)
+
+val_predictions = melb_model.predict(val_X)
+
+print "Mean Absolute Error = " + str(mean_absolute_error(val_predictions, val_y))
